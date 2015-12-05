f6382aafc2c304914f5b2b878c61e9533c95ca17
Felipe Tovar <FelipeTovarMac@Felipes-MacBook-Air.local>
2015-12-05 11:15:03 -0500

    Creation of project

---
+++ app/src/main/java/com/kogimobile/kogitest/fragments/BigImageFragment.java
@@ -0,0 +1,23 @@
+package com.kogimobile.kogitest.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.kogimobile.kogitest.R;
+
+/**
+ * Created by FelipeTovarMac on 12/5/15.
+ */
+public class BigImageFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View  rootView =  inflater.inflate(R.layout.big_image_fragment, container, false);
+
+        return rootView;
+    }
+}
